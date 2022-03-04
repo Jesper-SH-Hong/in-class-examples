8984e4f70f1db565aa437eca070e888b73dc3000
SUNG HYUN HONG <seonghyun91@gmail.com>
2022-03-04 14:54:14 -0800

    part 1 setup

---
+++ w5_JS/second-code.js
@@ -0,0 +1,17 @@
+f1 = function(){
+
+
+    console.log("Something!")
+}
+// 이딴 게 가능함..ㅋㅋㅋ
+
+
+function area(r) {
+    
+    result = r*r*22/7;  //22/7 is exact pi!!
+    console.log(result);
+
+}
+
+area(5)
+
